c6e8dbbea4418ebeda1c5c98f112460593ed6d8b
yepsson <32198001+yepsson@users.noreply.github.com>
2018-01-24 13:48:33 +0100

    paaaarser

--- parser.js
+++ parser.js
@@ -1,34 +1,74 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-class console {
-    log(input) {
-        process.stdout.write(input)
-        process.stdout.write('\n')
+class Parser {
+
+    constructor(filePath, action) {
+        if(filePath){
+
+            this.text = fs.readFileSync(filePath).toString();
+        }
+        else{
+            this.text = "";
+
+        }
+        this.action = action || this.defaultAction;
+
+        
+        this.parserMode = false;
+        
     }
-}
+    defaultAction(symbol){
 
-const text = fs.readFileSync('./index.html').toString();
+        return chalk.red(symbol);
 
-let redTextFlag = false;
+    }
 
-for (var i = 0; i < text.length; i++) {
-    let symbol = text[i];
 
-    switch(symbol) {
-        case "<": 
-            redTextFlag = true;
-            break;
-        case ">":
-            redTextFlag = false;
-            break;
-        default:
-            if (redTextFlag) {
-                symbol = chalk.red(symbol)
+    parse() {
+    
+        for (var i = 0; i < this.text.length; i++) {
+            let symbol = this.text[i];
+
+            switch (symbol) {
+                case "<":
+                    this.parserMode = true;
+                    break;
+                case ">":
+                    this.parserMode = false;
+                    break;
+
+                    case "/":
+                    
+                    if (this.parserMode == true){
+                        this.parserMode = false;
+                    }
+                    else {
+
+                        this.parserMode = true;
+
+                    }
+                    break;
+
+                
+
+                default:
+                    if (this.parserMode) {
+                        symbol = this.action(symbol)
+                    }
+                    process.stdout.write(symbol);
+                    break;
             }
-            process.stdout.write(symbol);
-            break;
+
+        }
+        process.stdout.write('\n');
     }
+
 }
 
-process.stdout.write('\n');
+
+var myParser = new Parser('./code.txt', (symbol) => {return chalk.green(symbol);});
+var myHTMLParser = new Parser('./index.html', (symbol) => {return chalk.yellow(symbol);});
+myParser.parse();
+myHTMLParser.parse();
+
